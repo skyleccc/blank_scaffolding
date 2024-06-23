fdd3dc87cdd2a007d5656a479fe1857d3cf39e2d
Justin Alec M. Antonio <justinantonio787@yahoo.com>
2024-06-23 15:31:41 +0800

    System Analytics and Design - ASSIGNMENT
    -- Made By: Justin Alec M. Antonio

---
+++ resources/js/components/forms/login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <b-row>
+      <b-col></b-col>
+      <b-col>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+          <b-form-group
+            id="input-group-1"
+            label="Email address:"
+            label-for="input-1"
+            description="We'll never share your email with anyone else."
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.email"
+              type="email"
+              placeholder="Enter email"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              v-model="form.password"
+              placeholder="Password"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="button" variant="primary" @click="register()">Sign Up</b-button>
+        </b-form>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      register(){
+        axios.post('/register/registration', this.form).then(response=>{
+          console.log(response.data.email);
+          console.log(response.data.password);
+        }).catch(error=>{
+          console.log(error.data);
+          this.loading = false;
+        })
+      },
+
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
